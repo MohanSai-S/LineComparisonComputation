a6e877f91c11c78dcf21481e7de43ed608b8102d
MohanSai-S <sathyanarayanamohansai.gmail.com>
2021-04-27 21:29:45 +0530

    Added : Welcome message

---
+++ src/LineComparisonComputation/LineComparison.java
@@ -0,0 +1,9 @@
+package LineComparisonComputation;
+
+public class LineComparison {
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Line comparison computation program");
+	}
+
+}
