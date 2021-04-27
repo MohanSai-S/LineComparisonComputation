c6686475992c84fbc30ad645c1a6c0f2b94ba4bb
MohanSai-S <sathyanarayanamohansai.gmail.com>
2021-04-27 23:21:43 +0530

    Added : Program Equality of two lines using Java equals Method

--- src/LineComparisonComputation/LineComparison.java
+++ src/LineComparisonComputation/LineComparison.java
@@ -1,26 +1,41 @@
 package LineComparisonComputation;
 
-import java.util.*;
+import java.util.Scanner;
 public class LineComparison {
 	
 	public static void main(String[] args) {
 		//Variables
-		double lengthOfALine=0;
+		double lengthOfLine1=0.0;
+		double lengthOfLine2=0.0;
 		//UserInputs
 		Scanner sc=new Scanner(System.in);
-		System.out.print("Enter the x1 value : ");
-		int x1 = sc.nextInt();
-		System.out.print("Enter the y1 value : ");
-		int y1 = sc.nextInt();
-		System.out.print("Enter the x2 value : ");
-		int x2 = sc.nextInt();
-		System.out.print("Enter the y2 value : ");
-		int y2 = sc.nextInt();
+		System.out.print("Enter the line1x1 value : ");
+		int line1x1 = sc.nextInt();
+		System.out.print("Enter the line1y1 value : ");
+		int line1y1 = sc.nextInt();
+		System.out.print("Enter the line1x2 value : ");
+		int line1x2 = sc.nextInt();
+		System.out.print("Enter the line1y2 value : ");
+		int line1y2 = sc.nextInt();
+		System.out.print("Enter the line2x1 value : ");
+		int line2x1 = sc.nextInt();
+		System.out.print("Enter the line2y1 value : ");
+		int line2y1 = sc.nextInt();
+		System.out.print("Enter the line2x2 value : ");
+		int line2x2 = sc.nextInt();
+		System.out.print("Enter the line2y2 value : ");
+		int line2y2 = sc.nextInt();
+		
 		
 		//Computations
 		
-		lengthOfALine=Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2));
-		System.out.println("Length Of the Line = " + lengthOfALine);
+		lengthOfLine1=Math.sqrt(Math.pow((line1x2-line1x1),2)+Math.pow((line1y2-line1y1),2));
+		lengthOfLine2=Math.sqrt(Math.pow((line2x2-line2x1),2)+Math.pow((line2y2-line2y1),2));
+		String line1Length=String.valueOf(lengthOfLine1);
+		String line2Length=String.valueOf(lengthOfLine2);
+		System.out.println("Length Of the Line1 = " + line1Length);
+		System.out.println("Length Of the Line2 = " + line2Length);
+		System.out.println("Equality of 2 lines is " + line1Length.equals(line2Length));		
 	}
 
 }
